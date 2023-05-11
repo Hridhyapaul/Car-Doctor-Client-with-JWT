51209032078ef552dd4853c6b00c92a70a48ac61
Your Name <hridhyapaul77@gmail.com>
2023-05-12 03:57:28 +0600

    Created AuthProvider, User and login user with firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCrF0bg77d8Py29PYAN_MTgU7jzUTZCzvc",
-  authDomain: "car-doctor-aef2d.firebaseapp.com",
-  projectId: "car-doctor-aef2d",
-  storageBucket: "car-doctor-aef2d.appspot.com",
-  messagingSenderId: "562876982043",
-  appId: "1:562876982043:web:42cf6d4ab586dc0978a9be"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
