30f2df6e649e7abd972216e59db7c6cb0ebd306b
Hridhyapaul <hpdesign2021@gmail.com>
2023-05-12 03:57:28 +0600

    Created AuthProvider, User and login user with firebase

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -3,25 +3,26 @@ import img from '../../assets/images/login/login.svg'
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../Provider/AuthProvider';
+import {AuthContext} from '../../Provider/AuthProvider';
 
-const Login = () => {
-    const {loginUser} = useContext(AuthContext)
-    const handleLogin = (event) => {
+const SignUp = () => {
+    const {createUser} = useContext(AuthContext)
+
+    const handleSignUp = (event) => {
         event.preventDefault();
         const form = event.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log (email, password)
-        loginUser(email, password)
+        console.log (name, email, password)
+
+        createUser(email, password)
         .then(result => {
-            const loggedUser = result.user;
-            console.log(loggedUser)
+            const signedUser = result.user;
+            console.log(signedUser)
             form.reset()
         })
-        .catch(error => {
-            console.log(error.message)
-        })
+        .catch(error => console.log(error.message))
     }
     return (
         <div>
@@ -32,28 +33,34 @@ const Login = () => {
                     </div>
                     <div className="card w-full max-w-xl shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <h1 className="text-5xl font-bold text-center">Login now!</h1>
-                            <form onSubmit={handleLogin}>
+                            <h1 className="text-5xl font-bold text-center">Sign Up</h1>
+                            <form onSubmit={handleSignUp}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-semibold">Name</span>
+                                    </label>
+                                    <input type="text" placeholder="Name" name="name" className="input input-bordered" required/>
+                                </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text font-semibold">Email</span>
                                     </label>
-                                    <input type="email" placeholder="Email" name="email" className="input input-bordered" />
+                                    <input type="email" placeholder="Email" name="email" className="input input-bordered" required/>
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text font-semibold">Password</span>
                                     </label>
-                                    <input type="password" name="password" placeholder="Password" className="input input-bordered" />
+                                    <input type="password" name="password" placeholder="Password" className="input input-bordered" required/>
                                     <label className="label">
                                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>
                                 </div>
                                 <div className="form-control mt-6">
-                                    <input className="btn btn-primary bg-[#FF3811] border-0 hover:bg-[#FF3811]" type="submit" value="Login" />
+                                    <input className="btn btn-primary bg-[#FF3811] border-0 hover:bg-[#FF3811]" type="submit" value="Sign Up" />
                                 </div>
                             </form>
-                            <p className='text-center mt-4 font-semibold'>Or Sign In With</p>
+                            <p className='text-center mt-4 font-semibold'>Or Sign Up With</p>
                             <div className='flex justify-center gap-4 mt-4'>
                                 <button className="btn btn-circle bg-slate-200 border-0 hover:bg-slate-200">
                                     <FcGoogle className='text-xl'></FcGoogle>
@@ -65,7 +72,7 @@ const Login = () => {
                                     <FaLinkedinIn className='text-[#0A66C2] text-xl'></FaLinkedinIn>
                                 </button>
                             </div>
-                            <p className='text-center mt-4'>New to Car Doctors? <Link to='/signUp' className='text-[#FF3811] font-bold'>Sign Up</Link></p>
+                            <p className='text-center mt-4'>Already Have an Account? <Link to='/login' className='text-[#FF3811] font-bold'>Sign In</Link></p>
                         </div>
                     </div>
                 </div>
@@ -74,4 +81,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
